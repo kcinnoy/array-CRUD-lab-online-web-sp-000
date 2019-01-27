08cfb2ef0b86b9332fdd7f721c1354abfab81d26
Yonnick Brown <25hour24@gmail.com>
2019-01-27 11:39:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,8 +34,3 @@ def retrieve_last_element_from_array(array)
   array.last
 end
 
-
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  def retrieve_element_from_index(array, index_number)
-    array[index_number]
-  end
